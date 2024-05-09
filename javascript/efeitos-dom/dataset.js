88f37153481aa416d21c31f322988a08e00c1df7
Marcos Vinicius Braga <marcos.vbragasouza@gmail.com>
2024-05-08 22:19:07 -0300

    finalização do módulo sobre DOM para iniciantes e organização de alguns arquivos.

---
+++ javascript/efeitos-dom/dataset.js
@@ -0,0 +1,21 @@
+/*
+=> DATASET é utilizado no DOM igual se utiliza CLASSE
+
+=> A vantagem de se utilizar data atributes é que torna mais fácil evitarmos conflitos com estilos do CSS. Além de deixar a estrutura da tag mais organizada.
+    <div data-anima="left" data-tempo="1000">Div 1</div>
+    <div class="anima-left tempo-1000">Div 2</div>
+
+=> NOMEMCLATURA
+    - Javascript não aceita "-" como nome de propriedade
+    - transforma a próxima letra em MAIÚSCULO
+    - Então qualquer traço será removido e a letra seguinte transformada em maiúscula.
+    - posso escrever o traço, mas tenho que saber que o JS irá transformar
+    - EXEMPLOS:
+        data-anima-scroll-up-teste ==> animaScrollUpTeste
+*/
+
+const h1 = document.querySelector('h1');
+
+console.log(Object.prototype.toString.call(h1));
+
+console.log(h1.dataset);
--- javascript/projetos-praticos/animais-fantasticos/script.js
+++ javascript/projetos-praticos/animais-fantasticos/script.js
@@ -1,51 +1,96 @@
-/*const animais = document.getElementById('animais');
-//console.log(animais);
+function initTabNav() { //SEMPRE BOM ISOLAR O ESCOPO PARA MELHOR ORGANIZAÇÃO
 
-const gridSection = document.getElementsByClassName('grid-section');
-//console.log(gridSection.length);
+    const tabMenu = document.querySelectorAll('.js-tabMenu li');
+    const tabContent = document.querySelectorAll('.js-tabContent section');
 
-const primeiraLi = document.querySelector('li');
-//console.log(primeiraLi);
+    if(tabMenu.length && tabContent.length) { //SÓ VAI ATIVAR SE ESSAS DUAS VARIÁVEIS EXISTIREM NO MEU CÓDIGO. 
+    tabContent[0].classList.add('ativo'); //FAZ COM QUE O CONTEÚDO ESTEJA ATIVO AO RECARREGAR A PÁGINA. SE NÃO, NÃO APARECE NENHUM CONTEÚDO POR CONTA DA FUNÇÃO ABAIXO.
 
-const primeiraUl = document.querySelector('ul');
-//console.log(primeiraUl);
+        function activeTab(index) { //ativa a classe de acordo com o número que for passado
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            })
+            tabContent[index].classList.add('ativo');
+        }
 
-const linksInternos = document.querySelector('[href^="#"]');
-//console.log(linksInternos);
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            })
+        }); 
+    }
+}
+initTabNav();
 
-const animaisImg = document.querySelectorAll('.animais img');
-//console.log(animaisImg[3]);
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt'); //seleciona a dt no HTML
+    const activeClass = 'ativo';
+    if(accordionList.length) {
+        accordionList[0].classList.add(activeClass); //FAZ COM QUE O PRIMEIRO ITEM JÁ FIQUE ABERTO
+        accordionList[0].nextElementSibling.classList.add(activeClass); //FAZ COM QUE O PRIMEIRO ITEM JÁ FIQUE ABERTO
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);//AO CLICAR NO DT, ADICIONA A class 'ativo' NO PRÓXIMO ITEM, OU SEJA, NO DD
+            //this => vai fazer referência ao item que estiver clicando no momento. 
+        }
 
-primeiraUl.classList.add('grid-section')
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        }); 
+    }
+    
+}
+initAccordion();
 
-const gridSectionHtml = document.getElementsByClassName('grid-section');
-const gridSectionNode = document.querySelectorAll('.grid-section');
-//console.log(gridSectionHtml);
-//console.log(gridSectionNode);
+function initScrollSuave() {
 
-gridSectionNode.forEach(function(item, index){
-    console.log(item);
-})
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-const arrayGrid = Array.from(gridSectionHtml)
-*/
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start', //alinha o bloco ao ínicio da seção
+        });
 
-/*const imgs = document.querySelectorAll('img');
 
-imgs.forEach(function(item, index, array){
-    //console.log(item, index, array);
-})
+        // //FAZ A ROLAGEM SUAVE. AINDA NÃO SUPORTADO EM TODOS OS BROWSERS
+        // const topo = section.offsetTop;
+        // window.scrollTo({ 
+        //     top: topo,
+        //     behavior: 'smooth'
+        // });
+    }
 
-const titulos = document.getElementsByClassName('titulo');
-const titulosArray = Array.from(titulos); //transforma o título em um ARRAY
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection); 
+    });
+}
+initScrollSuave();
 
-titulosArray.forEach(function(item, index, array){
-  console.log(item, index, array);
-});
-*/
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
 
-// const imgs = document.querySelectorAll('img');
+    if(sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
 
-// imgs.forEach((item) => {
-//     console.log(item);
-// })
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if(isSectionVisible) {
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            })
+        }
+
+        animaScroll()
+
+        window.addEventListener('scroll', animaScroll); 
+    } 
+}
+initAnimacaoScroll();
